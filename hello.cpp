f41882d64fd7ae3454914738fd6d1c2dccad6795
IKSohan <c253001@ugrad.iiuc.ac.bd>
2026-08-16 08:04:56 +0600

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,39 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello sohan " << endl;
+    int n,x,k,x1,f=0;
+    cin >> n;
+    for (int i = 0; i < n;i++)
+    {
+        for (int j = 0; j < n;j++)
+        {
+            cin >> x >> k;
+            if(x%k==0)
+            {
+                x1 = x - 1;
 
-    return 0;
+            }
+
+            if(x%k!=0)
+            
+            {
+                f = 1;
+            }
+
+        }
+
+
+        if(f!=1)
+        {
+            cout << 2 << endl
+                 << x1 << " " << x1 - x << endl;
+        }
+        else
+        {
+            cout << 1 << x << endl;
+            
+        }
+    }
+
+        return 0;
 }
